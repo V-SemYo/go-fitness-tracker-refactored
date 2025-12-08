93fdcd39e7d2ce9c48b4ac707157f05a18abf251
Иван Семенов <ran46@yandex.ru>
2025-12-08 21:13:36 +0300

    add Personal struct with Print method

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -1,9 +1,17 @@
 package personaldata
 
+import "fmt"
+
 type Personal struct {
-	// TODO: добавить поля
+	// Personal содержит персональные данные пользователя
+	Name   string
+	Weight float64
+	Height float64
 }
 
 func (p Personal) Print() {
-	// TODO: реализовать функцию
+	// Print выводит данные пользователя в консоль
+	fmt.Println("Имя: ", p.Name)
+	fmt.Println("Вес: ", p.Weight)
+	fmt.Println("Рост: ", p.Height)
 }
