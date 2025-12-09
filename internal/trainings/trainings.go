31a8922a366fcf75fde05bd879d7b28618e29a45
Иван Семенов <ran46@yandex.ru>
2025-12-09 10:31:38 +0300

    made structure Training and finished Parse method

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,11 +1,41 @@
 package trainings
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+)
+
 type Training struct {
-	// TODO: добавить поля
+	Steps        int
+	TrainingType string
+	Duration     time.Duration
+	personaldata.Personal
 }
 
+// Parse разбирает строку с данными о тренировке и сохраняет в структуру Training
 func (t *Training) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 3 {
+		return errors.New("Не правильный формат строки, нужно 3 части!")
+	}
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return errors.New("Не правильный формат количества шагов")
+	}
+	trainingType := parts[1]
+	duration, err := time.ParseDuration(parts[2])
+	if err != nil {
+		return errors.New("Не правильный формат продолжительности")
+	}
+	t.Steps = steps
+	t.TrainingType = trainingType
+	t.Duration = duration
+
+	return nil
 }
 
 func (t Training) ActionInfo() (string, error) {
