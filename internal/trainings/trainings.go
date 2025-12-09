70f015e96f5516e7173e13dac726b78b7c0cf7bd
Иван Семенов <ran46@yandex.ru>
2025-12-09 18:02:20 +0300

    complete trainings package with all methods

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -2,11 +2,13 @@ package trainings
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
 )
 
 type Training struct {
@@ -38,6 +40,26 @@ func (t *Training) Parse(datastring string) (err error) {
 	return nil
 }
 
+// ActionInfo формирует строку с информацией о тренировке
 func (t Training) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance := spentenergy.Distance(t.Steps, t.Height)
+	speed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
+
+	var calories float64
+	var err error
+
+	switch t.TrainingType {
+	case "Бег":
+		calories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	case "Ходьба":
+		calories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	default:
+		return "", errors.New("неизвестный тип тренировки")
+	}
+	if err != nil {
+		return "", err
+	}
+
+	info := fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f", t.TrainingType, t.Duration.Hours(), distance, speed, calories)
+	return info, nil
 }
