f005e63ea8277d99bfdf55a1eaf649e7f64475e0
Иван Семенов <ran46@yandex.ru>
2025-12-09 18:52:09 +0300

    finished daysteps package with ActionInfo method

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,11 +2,13 @@ package daysteps
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
 )
 
 type DaySteps struct {
@@ -34,6 +36,13 @@ func (ds *DaySteps) Parse(datastring string) (err error) {
 	return nil
 }
 
+// ActionInfo формирует строку с информацией о прогулке
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return "", err
+	}
+	info := fmt.Sprintf("Количество шагов: %d.\nДистанция составила: %.2f км.\nВы сожгли %.2f ккал.", ds.Steps, distance, calories)
+	return info, nil
 }
