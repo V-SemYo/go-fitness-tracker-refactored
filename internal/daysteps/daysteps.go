42c108c6e544d2954a577f31a885ab3019af0133
Иван Семенов <ran46@yandex.ru>
2025-12-09 18:31:01 +0300

    made DaySteps struct finished Parse method

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,11 +1,37 @@
 package daysteps
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
+// Parse разбирает строку с данными о прогулке
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		return errors.New("Не правильный формат строки, нужно 2 части!")
+	}
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return errors.New("Не правильный формат количества шагов")
+	}
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return errors.New("Не правильный формат продолжительности")
+	}
+	ds.Steps = steps
+	ds.Duration = duration
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
