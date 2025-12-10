ae0eafe2f6e74b86696855cfa49b1a228b036538
Иван Семенов <ran46@yandex.ru>
2025-12-10 10:55:29 +0300

    updated Personal Print() method after failed git-test

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -11,7 +11,7 @@ type Personal struct {
 
 func (p Personal) Print() {
 	// Print выводит данные пользователя в консоль
-	fmt.Println("Имя: ", p.Name)
-	fmt.Println("Вес: ", p.Weight)
-	fmt.Println("Рост: ", p.Height)
+	fmt.Printf("Имя: %s\n", p.Name)
+	fmt.Printf("Вес: %.2f кг.\n", p.Weight)
+	fmt.Printf("Рост: %.2f м.\n", p.Height)
 }
